b639f2e838915c659bbe0b74b3f0a107638bf1e9
Andrew Allshouse <andrew.allshouse@ddiworld.com>
2023-01-23 19:50:49 -0500

    update test

--- helloworld_internal_test.go
+++ helloworld_internal_test.go
@@ -1,6 +1,13 @@
 package main
 
-func ExampleMain() {
-	main()
+import "testing"
+
+func TestGreet(t *testing.T) {
+	expectedGreeting := "Hello, World!"
+	greeting := greet()
+
+	if greeting != expectedGreeting {
+		t.Errorf("expected: %q, got: %q", greeting, expectedGreeting)
+	}
 	// Output: Hello, World!
 }
